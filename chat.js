a0f55e10657669a09b56117b710e1704965cd3dd
Valmir Almeida <68277445+valmir1227@users.noreply.github.com>
2022-01-29 02:07:08 -0300

    aula 3
    Mensagensno chat

--- chat.js
+++ chat.js
@@ -3,18 +3,29 @@ import React from "react";
 import appConfig from "../pages/config.json";
 
 export default function ChatPage() {
-  // Sua lógica vai aqui
+  /* 
+  (!) Usuário(a)
+  - Usuário digita no campo textarea  
+  - Aperta enter para enviar
+  - Tem que adicionar o texto na listagem
+
+  (!) Dev
+  - Criar campo
+  - Usar onchage e usestate (if  para limpar variável)
+  - Lista de mensagem
+*/
+
   const [mensagem, setMensagem] = React.useState("");
   const [listaDeMensagem, setListaDeMensagens] = React.useState([]);
-  // ./Sua lógica vai aqui
+
   function handleNovaMensagem(novaMensagem) {
     const mensagem = {
       id: listaDeMensagem.length + 1,
-      de: "Valmir",
+      de: "valmir",
       texto: novaMensagem,
     };
 
-    setListaDeMensagens([...listaDeMensagem, mensagem]);
+    setListaDeMensagens([mensagem, ...listaDeMensagem]);
   }
 
   return (
@@ -39,11 +50,17 @@ export default function ChatPage() {
           flex: 1,
           boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
           borderRadius: "5px",
-          backgroundColor: appConfig.theme.colors.neutrals[700],
+
+          background: "rgba(199, 159, 67, 0.3)",
+          boxshadow: "0 4px 30px rgba(0, 0, 0, 0)",
+          backdropfilter: "blur(3.3px)",
+          border: "1px solid rgba(199, 159, 67,1)",
+
           height: "100%",
-          maxWidth: "95%",
+          maxWidth: "75%",
           maxHeight: "95vh",
           padding: "32px",
+          overflow: "hidden",
         }}
       >
         <Header />
@@ -51,22 +68,26 @@ export default function ChatPage() {
           styleSheet={{
             position: "relative",
             display: "flex",
+            justifyContent: "flex-end",
+            border: "1px solid rgba(199, 159, 67,1)",
             flex: 1,
             height: "80%",
-            backgroundColor: appConfig.theme.colors.neutrals[600],
+            backgroundColor: appConfig.theme.colors.neutrals[900],
             flexDirection: "column",
             borderRadius: "5px",
             padding: "16px",
           }}
         >
-          {/*<MessageList mensagens={[]} /> */}
-          {listaDeMensagem.map((mensagemAtual) => {
+          <MessageList mensagem={listaDeMensagem} />
+
+          {/*{listaDeMensagem.map((mensagemAtual) => {
             return (
               <li key={mensagemAtual.id}>
                 {mensagemAtual.de}: {mensagemAtual.texto}
               </li>
             );
-          })}
+          })} */}
+
           <Box
             as="form"
             styleSheet={{
@@ -122,7 +143,7 @@ function Header() {
         <Text variant="heading5">Chat</Text>
         <Button
           variant="tertiary"
-          colorVariant="neutral"
+          colorVariant="dark"
           label="Logout"
           href="/"
         />
@@ -132,12 +153,12 @@ function Header() {
 }
 
 function MessageList(props) {
-  console.log(props.listaDeMensagem);
   return (
     <Box
       tag="ul"
       styleSheet={{
-        overflow: "scroll",
+        overflow: "auto",
+
         display: "flex",
         flexDirection: "column-reverse",
         flex: 1,
@@ -145,47 +166,51 @@ function MessageList(props) {
         marginBottom: "16px",
       }}
     >
-      <Text
-        key={mensagem.id}
-        tag="li"
-        styleSheet={{
-          borderRadius: "5px",
-          padding: "6px",
-          marginBottom: "12px",
-          hover: {
-            backgroundColor: appConfig.theme.colors.neutrals[700],
-          },
-        }}
-      >
-        <Box
-          styleSheet={{
-            marginBottom: "8px",
-          }}
-        >
-          <Image
-            styleSheet={{
-              width: "20px",
-              height: "20px",
-              borderRadius: "50%",
-              display: "inline-block",
-              marginRight: "8px",
-            }}
-            src={`https://github.com/vanessametonini.png`}
-          />
-          <Text tag="strong">{mensagem.de}</Text>
+      {props.mensagem.map((mensagem) => {
+        return (
           <Text
+            key={mensagem.id}
+            tag="li"
             styleSheet={{
-              fontSize: "10px",
-              marginLeft: "8px",
-              color: appConfig.theme.colors.neutrals[300],
+              borderRadius: "5px",
+              padding: "6px",
+              marginBottom: "12px",
+              hover: {
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+              },
             }}
-            tag="span"
           >
-            {new Date().toLocaleDateString()}
+            <Box
+              styleSheet={{
+                marginBottom: "8px",
+              }}
+            >
+              <Image
+                styleSheet={{
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                  display: "inline-block",
+                  marginRight: "8px",
+                }}
+                src="https://github.com/valmir1227.png"
+              />
+              <Text tag="strong">{mensagem.de}</Text>
+              <Text
+                styleSheet={{
+                  fontSize: "10px",
+                  marginLeft: "8px",
+                  color: appConfig.theme.colors.neutrals[300],
+                }}
+                tag="span"
+              >
+                {new Date().toLocaleDateString()}
+              </Text>
+            </Box>
+            {mensagem.texto}
           </Text>
-        </Box>
-        {mensagem.texto}
-      </Text>
+        );
+      })}
     </Box>
   );
 }
